eaff225b1430bc8aa19eb5a0c250866c3277d4e9
Vincent Wheeler <vincent.wheeler@anu.edu.au>
2020-01-12 20:28:54 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import kivy
+
+kivy.require('1.11.1')
+#print(kivy.__version__)
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
+)
+import numpy as np
+
+
+class PressureSensor(Widget):
+    
+    data = StringProperty(10)
+
+    def pull(self):
+        newdat = 10 + np.random.randn()
+        self.data = "{:.3f}".format(newdat)
+        
+
+class FrictionDisplay(Widget):
+
+    ps1 = ObjectProperty(None)
+    ps2 = ObjectProperty(None)
+    
+    def update(self,dt):
+        self.ps1.pull()
+        self.ps2.pull()
+    
+
+
+class FrictionTrainerApp(App):
+
+    def build(self):
+        print('\n\n\n*********building**********\n\n\n')
+        display = FrictionDisplay()
+        Clock.schedule_interval(display.update, 30.0/60.0)
+        return display
+
+
+if __name__ == '__main__':
+    FrictionTrainerApp().run()
